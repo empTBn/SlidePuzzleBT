750443e5cbea1c7286ccf68c324ad373b0aafba7
Matemm <85372632+Etcesc@users.noreply.github.com>
2023-09-11 16:20:26 -0600

    Retoques + mover pieza (revisar)
    Actualizado formato de celda vacía, agregadas funciones move piece y win condition (verificar)

--- puzzle.js
+++ puzzle.js
@@ -7,8 +7,16 @@ let imagePieces = [];
 startButton.addEventListener('click', () => {
     const size = parseInt(boardSizeInput.value);
     const selectedImage = imageOptions.value;
-    createPuzzleBoard(size, selectedImage);
+    createPuzzleBoard(size, selectedImdage);
 });
+
+const emptyCell = {
+    cell:{},
+    x:0,
+    y:0
+}
+
+const cellsList = []
 
 function createPuzzleBoard(size, selectedImage) {
     board.innerHTML = '';
@@ -32,8 +40,8 @@ function createPuzzleBoard(size, selectedImage) {
                 cell.style.width = `${imageWidth / size}px`;
                 cell.style.height = `${imageHeight / size}px`;
 
-                const offsetX = (imageWidth / size) * x;
-                const offsetY = (imageHeight / size) * y;
+                emptyCell.x = Math.floor(Math.random() * size);
+                emptyCell.y = Math.floor(Math.random() * size);
 
                 if (x === emptyCellX && y === emptyCellY) {
                     // Crea un espacio vacío en la posición aleatoria
@@ -42,7 +50,9 @@ function createPuzzleBoard(size, selectedImage) {
                     cell.style.backgroundImage = `url(${selectedImage})`;
                     cell.style.backgroundSize = `${imageWidth}px ${imageHeight}px`;
                     cell.style.backgroundPosition = `-${offsetX}px -${offsetY}px`;
+                    cell.addEventListener('click', () => movePiece(cell, size));
                 }
+                cellsList.push({cell, x, y})
 
                 board.appendChild(cell);
             }
@@ -51,6 +61,55 @@ function createPuzzleBoard(size, selectedImage) {
         // Llama a la función shuffleArray para mezclar las celdas aleatoriamente
         shuffleArray(Array.from(board.querySelectorAll('.grid-cell')));
     };
+}
+
+function movePiece(cell, size) {
+    const emptyCellX = emptyCell.x;
+    const emptyCellY = emptyCell.y;
+
+
+    let cellInfo = {}
+
+    for(let i = 0; i < cellsList.length; i++){
+        if(cellsList[i].cell == cell){
+            cellInfo = cellsList[i]
+        }
+    }
+
+    console.log(cellInfo)
+
+    if ((Math.abs(cellInfo.x - emptyCellX) === 1 && cellInfo.y === emptyCellY) ||
+        (Math.abs(cellInfo.y - emptyCellY) === 1 && cellInfo.X === emptyCellX)) {
+        // Intercambia la posición de la celda clicada y la celda vacía
+        const offsetX = (imageWidth / size) * emptyCellX;
+        const offsetY = (imageHeight / size) * emptyCellY;
+        
+
+        emptyCell.x = cellInfo.x
+        emptyCell.y = cellInfo.y
+        // Verifica si se ha completado el rompecabezas
+        if (isPuzzleComplete(size)) {
+            alert('¡Felicidades! Has completado el rompecabezas.');
+        }
+    }
+}
+
+
+// Función para verificar si se ha completado el rompecabezas
+function isPuzzleComplete(size) {
+    const cells = board.querySelectorAll('.grid-cell');
+    let correct = true;
+
+    cells.forEach((cell, index) => {
+        const cellX = parseInt(cell.style.gridColumn);
+        const cellY = parseInt(cell.style.gridRow);
+
+        if (cellX !== (index % size) + 1 || cellY !== Math.floor(index / size) + 1) {
+            correct = false;
+        }
+    });
+
+    return correct;
 }
 
 // Función para mezclar un array aleatoriamente (shuffle)
