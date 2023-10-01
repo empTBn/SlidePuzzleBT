0d594b80b1c5365eeccfebc66e34ef385a5b443f
sebascrugu <sebastiancruz@estudiantec.cr>
2023-09-30 19:39:50 -0600

    update a.js

--- a.js
+++ a.js
@@ -1,120 +1,132 @@
-class PuzzleNode {
-    constructor(board, emptyCell, moves, parent) {
-        this.board = board; // Representación actual del tablero
-        this.emptyCell = emptyCell; // Posición de la celda vacía
-        this.moves = moves; // Número de movimientos realizados hasta este nodo
-        this.parent = parent; // Nodo padre
-        this.heuristic = this.calculateHeuristic(); // Valor heurístico (número de fichas mal colocadas)
+/**
+ * Representa un nodo en el algoritmo A*.
+ */
+class Node {
+    constructor(data, level, fval) {
+        this.data = data;
+        this.level = level;
+        this.fval = fval;
     }
 
-    // Calcula la heurística (número de fichas mal colocadas)
-    calculateHeuristic() {
-        let misplacedTiles = 0;
-        for (const cell of this.board) {
-            if (cell.x !== cell.target.x || cell.y !== cell.target.y) {
-                misplacedTiles++;
+    /**
+     * Genera los nodos hijos del nodo actual moviendo el espacio en blanco en las cuatro direcciones.
+     */
+    generateChild() {
+        const [x, y] = this.find(this.data, '_');
+        const valList = [
+            [x, y - 1],
+            [x, y + 1],
+            [x - 1, y],
+            [x + 1, y]
+        ];
+        const children = [];
+        for (const [i, j] of valList) {
+            const child = this.shuffle(this.data, x, y, i, j);
+            if (child !== null) {
+                const childNode = new Node(child, this.level + 1, 0);
+                children.push(childNode);
             }
         }
-        return misplacedTiles;
+        return children;
     }
 
-    // Calcula el costo total del nodo (g + h)
-    getTotalCost() {
-        return this.moves + this.heuristic;
+    /**
+     * Mueve el espacio en blanco en la dirección especificada y devuelve el nuevo estado del rompecabezas.
+     */
+    shuffle(puz, x1, y1, x2, y2) {
+        if (x2 >= 0 && x2 < puz.length && y2 >= 0 && y2 < puz[0].length) {
+            const tempPuz = this.copy(puz);
+            const temp = tempPuz[x2][y2];
+            tempPuz[x2][y2] = tempPuz[x1][y1];
+            tempPuz[x1][y1] = temp;
+            return tempPuz;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Realiza una copia profunda de una matriz.
+     */
+    copy(root) {
+        const temp = [];
+        for (const row of root) {
+            temp.push([...row]);
+        }
+        return temp;
+    }
+
+    /**
+     * Encuentra la posición del espacio en blanco en el rompecabezas.
+     */
+    find(puz, x) {
+        for (let i = 0; i < puz.length; i++) {
+            for (let j = 0; j < puz[i].length; j++) {
+                if (puz[i][j] === x) {
+                    return [i, j];
+                }
+            }
+        }
     }
 }
 
-class AStarSolver {
-    constructor(board, emptyCell) {
-        this.board = board; // Representación actual del tablero
-        this.emptyCell = emptyCell; // Posición de la celda vacía
-        this.size = Math.sqrt(board.length); // Tamaño del tablero (asumiendo que es cuadrado)
+/**
+ * Implementación del algoritmo A* para resolver el rompecabezas.
+ */
+class PuzzleSolver {
+    constructor(size) {
+        this.size = size;
+        this.open = [];
+        this.closed = [];
     }
 
-    // Encuentra la posición de una celda en el tablero
-    findCellPosition(cell) {
-        return cell.x * this.size + cell.y;
-    }
-
-    // Genera los movimientos válidos a partir de un estado dado
-    generateValidMoves(node) {
-        const validMoves = [];
-        const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // Movimientos arriba, derecha, abajo, izquierda
-
-        for (const dir of directions) {
-            const newX = node.emptyCell.x + dir[0];
-            const newY = node.emptyCell.y + dir[1];
-
-            if (newX >= 0 && newX < this.size && newY >= 0 && newY < this.size) {
-                const emptyPos = this.findCellPosition(node.emptyCell);
-                const newBoard = [...node.board]; // Clonar el tablero actual
-                // Intercambiar la celda vacía con la celda adyacente
-                [newBoard[emptyPos], newBoard[this.findCellPosition({ x: newX, y: newY })]] =
-                    [newBoard[this.findCellPosition({ x: newX, y: newY })], newBoard[emptyPos]];
-                validMoves.push(new PuzzleNode(newBoard, { x: newX, y: newY }, node.moves + 1, node));
-            }
-        }
-        return validMoves;
-    }
-
-    // Resuelve el rompecabezas utilizando el algoritmo A*
-    solve() {
-        const startNode = new PuzzleNode(this.board, this.emptyCell, 0, null);
-        const openSet = [startNode];
-        const closedSet = new Set();
-
-        while (openSet.length > 0) {
-            // Ordenar la lista de nodos por el costo total (f = g + h)
-            openSet.sort((a, b) => a.getTotalCost() - b.getTotalCost());
-
-            const currentNode = openSet.shift();
-
-            // Verificar si hemos llegado al estado objetivo
-            if (currentNode.heuristic === 0) {
-                // Reconstruir el camino hacia la solución
-                const solutionPath = [];
-                let current = currentNode;
-                while (current !== null) {
-                    solutionPath.push(current.board);
-                    current = current.parent;
-                }
-                solutionPath.reverse();
-                console.log('Se encontró una solución:');
-                console.log(`Movimientos realizados: ${solutionPath.length - 1}`);
-                for (let i = 1; i < solutionPath.length; i++) {
-                    console.log(`Movimiento ${i}:`);
-                    // Imprimir el tablero en cada paso del camino
-                    for (let row = 0; row < this.size; row++) {
-                        console.log(solutionPath[i]
-                            .slice(row * this.size, (row + 1) * this.size)
-                            .map(cell => `(${cell.x},${cell.y})`)
-                            .join(' '));
-                    }
-                }
-                return;
-            }
-
-            // Marcar el nodo actual como visitado
-            closedSet.add(JSON.stringify(currentNode.board));
-
-            // Generar movimientos válidos desde el nodo actual
-            const validMoves = this.generateValidMoves(currentNode);
-
-            for (const move of validMoves) {
-                const key = JSON.stringify(move.board);
-                if (!closedSet.has(key)) {
-                    openSet.push(move);
+    /**
+     * Calcula la heurística h(x) que representa la diferencia entre el rompecabezas actual y el objetivo.
+     */
+    h(start, goal) {
+        let temp = 0;
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (start[i][j] !== goal[i][j] && start[i][j] !== '_') {
+                    temp += 1;
                 }
             }
         }
+        return temp;
+    }
 
-        console.log('No se encontró una solución.');
+    /**
+     * Calcula el valor f(x) utilizado en el algoritmo A* (f(x) = h(x) + g(x)).
+     */
+    f(start, goal) {
+        return this.h(start, goal) + start.level;
+    }
+
+    /**
+     * Resuelve el rompecabezas utilizando el algoritmo A*.
+     */
+    solve(start, goal) {
+        start = new Node(start, 0, 0);
+        start.fval = this.f(start, goal);
+        this.open.push(start);
+
+        while (true) {
+            const cur = this.open[0];
+
+            if (this.h(cur.data, goal) === 0) {
+                break;
+            }
+
+            for (const child of cur.generateChild()) {
+                child.fval = this.f(child, goal);
+                this.open.push(child);
+            }
+
+            this.closed.push(cur);
+            this.open.shift();
+
+            this.open.sort((a, b) => a.fval - b.fval);
+        }
     }
 }
-
-// Event listener para el botón de resolución con A*
-aEstrellaButton.addEventListener('click', () => {
-    const aStarSolver = new AStarSolver(cellObjets, emptyCell);
-    aStarSolver.solve();
-});
 
