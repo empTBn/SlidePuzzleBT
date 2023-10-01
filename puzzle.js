bd4812f68c66f315595326cd12007f9cb0f7d914
julianrodzs <49877813+julianrodzs@users.noreply.github.com>
2023-09-30 18:58:51 -0600

    .

--- puzzle.js
+++ puzzle.js
@@ -464,22 +464,19 @@ backTrackingButton.addEventListener('click', () => {
 })
 
 
-/**
- * Clase que representa un nodo en el espacio de búsqueda del rompecabezas.
- */
 class PuzzleNode {
-    constructor(board, emptyCell, moves) {
-        this.board = board;
-        this.emptyCell = emptyCell;
-        this.moves = moves;
-        this.heuristic = this.calculateHeuristic();
+    constructor(board, emptyCell, moves, parent) {
+        this.board = board; // Representación actual del tablero
+        this.emptyCell = emptyCell; // Posición de la celda vacía
+        this.moves = moves; // Número de movimientos realizados hasta este nodo
+        this.parent = parent; // Nodo padre
+        this.heuristic = this.calculateHeuristic(); // Valor heurístico (número de fichas mal colocadas)
     }
 
     // Calcula la heurística (número de fichas mal colocadas)
     calculateHeuristic() {
         let misplacedTiles = 0;
-        for (let i = 0; i < this.board.length; i++) {
-            const cell = this.board[i];
+        for (const cell of this.board) {
             if (cell.x !== cell.target.x || cell.y !== cell.target.y) {
                 misplacedTiles++;
             }
@@ -493,26 +490,16 @@ class PuzzleNode {
     }
 }
 
-/**
- * Resuelve el rompecabezas utilizando el algoritmo A*.
- */
 class AStarSolver {
-
     constructor(board, emptyCell) {
-        this.board = board;
-        this.emptyCell = emptyCell;
-        this.size = Math.sqrt(this.board.length);
+        this.board = board; // Representación actual del tablero
+        this.emptyCell = emptyCell; // Posición de la celda vacía
+        this.size = Math.sqrt(board.length); // Tamaño del tablero (asumiendo que es cuadrado)
     }
 
-    // Encuentra la posición del nodo vacío en el tablero
-    findEmptyCellPosition(board) {
-        for (let i = 0; i < board.length; i++) {
-            const cell = board[i];
-            if (cell.x === this.emptyCell.x && cell.y === this.emptyCell.y) {
-                return i;
-            }
-        }
-        return -1;
+    // Encuentra la posición de una celda en el tablero
+    findCellPosition(cell) {
+        return cell.x * this.size + cell.y;
     }
 
     // Genera los movimientos válidos a partir de un estado dado
@@ -525,11 +512,12 @@ class AStarSolver {
             const newY = node.emptyCell.y + dir[1];
 
             if (newX >= 0 && newX < this.size && newY >= 0 && newY < this.size) {
-                const index = this.findEmptyCellPosition(node.board);
-                const newBoard = node.board.slice(); // Clonar el tablero actual
-                newBoard[index] = node.board[newX * this.size + newY];
-                newBoard[newX * this.size + newY] = this.emptyCell;
-                validMoves.push(new PuzzleNode(newBoard, { x: newX, y: newY }, node.moves + 1));
+                const emptyPos = this.findCellPosition(node.emptyCell);
+                const newBoard = [...node.board]; // Clonar el tablero actual
+                // Intercambiar la celda vacía con la celda adyacente
+                [newBoard[emptyPos], newBoard[this.findCellPosition({ x: newX, y: newY })]] =
+                    [newBoard[this.findCellPosition({ x: newX, y: newY })], newBoard[emptyPos]];
+                validMoves.push(new PuzzleNode(newBoard, { x: newX, y: newY }, node.moves + 1, node));
             }
         }
         return validMoves;
@@ -537,25 +525,44 @@ class AStarSolver {
 
     // Resuelve el rompecabezas utilizando el algoritmo A*
     solve() {
-        const startNode = new PuzzleNode(this.board, this.emptyCell, 0);
+        const startNode = new PuzzleNode(this.board, this.emptyCell, 0, null);
         const openSet = [startNode];
         const closedSet = new Set();
 
         while (openSet.length > 0) {
             // Ordenar la lista de nodos por el costo total (f = g + h)
             openSet.sort((a, b) => a.getTotalCost() - b.getTotalCost());
-
+  
             const currentNode = openSet.shift();
 
             // Verificar si hemos llegado al estado objetivo
             if (currentNode.heuristic === 0) {
-                alert("¡Has completado el rompecabezas en ${currentNode.moves} movimientos!");
+                // Reconstruir el camino hacia la solución
+                const solutionPath = [];
+                let current = currentNode;
+                while (current !== null) {
+                    solutionPath.push(current.board);
+                    current = current.parent;
+                }
+                solutionPath.reverse();
+                console.log('Se encontró una solución:');
+                console.log(`Movimientos realizados: ${solutionPath.length - 1}`);
+                for (let i = 1; i < solutionPath.length; i++) {
+                    console.log(`Movimiento ${i}:`);
+                    // Imprimir el tablero en cada paso del camino
+                    for (let row = 0; row < this.size; row++) {
+                        console.log(solutionPath[i]
+                            .slice(row * this.size, (row + 1) * this.size)
+                            .map(cell => `(${cell.x},${cell.y})`)
+                            .join(' '));
+                    }
+                }
                 return;
             }
 
             // Marcar el nodo actual como visitado
             closedSet.add(JSON.stringify(currentNode.board));
-
+            console.log(closedSet);
             // Generar movimientos válidos desde el nodo actual
             const validMoves = this.generateValidMoves(currentNode);
 
@@ -567,12 +574,12 @@ class AStarSolver {
             }
         }
 
-        alert("No existe solución para este rompecabezas.");
+        console.log('No se encontró una solución.');
     }
 }
 
 // Event listener para el botón de resolución con A*
 aEstrellaButton.addEventListener('click', () => {
-    const aStarSolver = new AStarSolver(cellObjets, specialCell);
+    const aStarSolver = new AStarSolver(cellObjets, emptyCell);
     aStarSolver.solve();
 });
